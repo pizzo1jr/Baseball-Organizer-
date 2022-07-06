017095851e4e3c9dfaa7c7fba3791f24a11a11ec
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-06 19:39:51 -0400

    set up the handlebars as view engine

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const sequelize = require('./config/connection'); // get the connection for database
 const express = require('express'); // import express library
-const routes = require('./controllers');
+const routes = require('./controllers'); // import all the routes from the controllers
+const exhbs = require('express-handlebars'); // import express handlebars library
+const hbs = exhbs.create({}) // create an instance of express handlebars
 const session = require('express-session') // import express-session
+const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store); //import classes to store user sessions.
 
 // make an instance of the session storage
@@ -20,8 +23,11 @@ const app = express(); // set app as an instance of express
 const PORT = process.env.PORT | 3001 // use environment port use port 3001 in local environment
 
 // middlewears for converting JSON to other data format
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // convert incoming req from JSON to javascript objects
 app.use(express.urlencoded({extended: true}));// parse object
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(session(sess)); // turn on the session
 app.use(routes); // turn on all routes
 
