576d852b60ef1a4ae93a90fb34207451c149567e
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-06 20:00:48 -0400

    added sign up form page

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {User} = require('../models');
+
+// render the log in page
+router.get('/user/login', (req, res)=>{
+   res.render('login');
+});
+
+// render the signup page
+router.get('/user/signup', (req, res)=>{
+   res.render('signup');
+});
+
+module.exports = router;
+
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 // get all the routes
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 
 // mount the routes
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
