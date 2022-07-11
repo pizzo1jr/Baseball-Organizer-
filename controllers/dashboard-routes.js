80d1a8aa1df322f71d3ac68f0ec43694482cdfb3
Anthony Dcosta <adcosta2000@gmail.com>
2022-07-10 23:54:58 -0400

    renders team list in my teams section. Added edit badge to it

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,21 @@ const router = require('express').Router();
 const {User, Player, Team} = require('../models');
 
 router.get('/', (req, res) => {
-   res.render('dashboard');
+   User.findOne({
+      where:{
+         id:req.session.user_id
+      },
+      include:[
+         {
+            model:Team,
+            attributes:['team_name']
+         }
+      ]
+   })
+   .then(dbUserData => {
+      const user = dbUserData.get({plain: true});
+      res.render('dashboard', {user});
+   })
 })
 
 module.exports = router;
